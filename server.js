8e11dea1d4c310b9dc75edad535ebe7c219105a7
Cyanoptilas <februarys22@gmail.com>
2023-12-29 23:11:42 +0900

    ADD 外部ファイル追加

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const http = require("http");
 const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
 
 // webサーバーを作ろう
-const server = http.createServer((rq, res) => {
+const server = http.createServer((req, res) => {
   // ブラウザからアクセルが来たときの処理
   res.writeHead(200, { "Content-Type": "text/html" });
   //   writeHead(statusCode: number, statusMessage?: string | undefined, headers?: http.OutgoingHttpHeaders | http.OutgoingHttpHeader[] | undefined): http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }
-  res.write("<h1>Hello Nodejs");
+  res.write(html);
   res.end();
+  if (req.method === "GET") console.log("GET");
+  if (req.method === "PUT") console.log("PUT");
 });
 
-server.listen(PORT, ()=>{
-    console.log("server started!!!!!!!!!!!")
+server.listen(PORT, () => {
+  console.log("server started!!!!!!!!!!!");
 });
