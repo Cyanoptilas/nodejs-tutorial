733fb191d7c9d6c484f1a0983d34a637348e6944
Cyanoptilas <februarys22@gmail.com>
2023-12-29 22:59:23 +0900

    ADD サーバー起動処理追加

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const http = require("http");
+const PORT = 8000;
+
+// webサーバーを作ろう
+const server = http.createServer((rq, res) => {
+  // ブラウザからアクセルが来たときの処理
+  res.writeHead(200, { "Content-Type": "text/html" });
+  //   writeHead(statusCode: number, statusMessage?: string | undefined, headers?: http.OutgoingHttpHeaders | http.OutgoingHttpHeader[] | undefined): http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }
+  res.write("<h1>Hello Nodejs");
+  res.end();
+});
+
+server.listen(PORT, ()=>{
+    console.log("server started!!!!!!!!!!!")
+});
